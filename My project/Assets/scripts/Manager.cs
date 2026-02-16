7f1ae72e61ae183c4c4252812f79ab5507189bd0
masmile8 <mafrassetto6@gmail.com>
2026-02-16 10:04:02 -0500

    add new scene and started script

---
+++ My project/Assets/scripts/Manager.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine;
+
+public class moveToAcross : MonoBehaviour
+{
+    [SerializeField] private GameObject Prefab;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    //after x time passes
+    //spawn obj 1(it has onstart code to move and play animations)
+    //loop till end
+
+}
